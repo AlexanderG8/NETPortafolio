5f23b9cba57596b2d45bacd859ae6271ed986d1a
Alexander Gomez <mpalexanderg@gmail.com>
2026-01-21 18:26:20 -0500

    Se agrego repository

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,6 +1,7 @@
 using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
 using NETPortafolio.Models;
+using NETPortafolio.Services;
 
 namespace NETPortafolio.Controllers
 {
@@ -11,6 +12,14 @@ namespace NETPortafolio.Controllers
      */
     public class HomeController : Controller
     {
+        private readonly ILogger _logger;
+        private readonly RepositoryProyectos _repositoryProyectos;
+
+        public HomeController(ILogger logger, RepositoryProyectos repositoryProyectos)
+        {
+            _logger = logger;
+            _repositoryProyectos = repositoryProyectos;
+        }
         /*
          * IActionResult son las funciones que se ejecutan cuando hacemos una petición HTTP a una ruta específica.
          */
@@ -19,45 +28,12 @@ namespace NETPortafolio.Controllers
             /*
              * View() es un método auxiliar que devuelve una vista asociada a la acción del controlador.
              */
-            var proyectos = ObtenerProyectos().Take(3).ToList();
+            var proyectos = _repositoryProyectos.ObtenerProyectos().Take(3).ToList();
             var modelo = new HomeIndexViewModel() { Proyectos = proyectos };
             return View(modelo);
         }
 
-        private List<Proyecto> ObtenerProyectos() 
-        {
-            return new List<Proyecto>()
-            {
-                new Proyecto
-                {
-                    Titulo = "Amazon",
-                    Descripcion = "E-Commerce realizado en ASP.NET Core.",
-                    ImagenURL = "/img/amazon.png",
-                    Link = "https://amazon.com"
-                },
-                new Proyecto
-                {
-                    Titulo = "New York Times",
-                    Descripcion = "Pagina de Noticias React.",
-                    ImagenURL = "/img/nyt.png",
-                    Link = "https://nytimes.com"
-                },
-                new Proyecto
-                {
-                    Titulo = "Reddit",
-                    Descripcion = "Red social para compartir en comunidades.",
-                    ImagenURL = "/img/reddit.png",
-                    Link = "https://reddit.com"
-                },
-                new Proyecto
-                {
-                    Titulo = "Steam",
-                    Descripcion = "Tienda en línea para comprar videojuegos.",
-                    ImagenURL = "/img/steam.png",
-                    Link = "https://store.steampowered.com"
-                }
-            };
-        }
+        
 
         public IActionResult Privacy()
         {
---
+++ Services/RepositoryProyectos.cs
@@ -0,0 +1,42 @@
+﻿using NETPortafolio.Models;
+
+namespace NETPortafolio.Services
+{
+    public class RepositoryProyectos
+    {
+        public List<Proyecto> ObtenerProyectos()
+        {
+            return new List<Proyecto>()
+            {
+                new Proyecto
+                {
+                    Titulo = "Amazon",
+                    Descripcion = "E-Commerce realizado en ASP.NET Core.",
+                    ImagenURL = "/img/amazon.png",
+                    Link = "https://amazon.com"
+                },
+                new Proyecto
+                {
+                    Titulo = "New York Times",
+                    Descripcion = "Pagina de Noticias React.",
+                    ImagenURL = "/img/nyt.png",
+                    Link = "https://nytimes.com"
+                },
+                new Proyecto
+                {
+                    Titulo = "Reddit",
+                    Descripcion = "Red social para compartir en comunidades.",
+                    ImagenURL = "/img/reddit.png",
+                    Link = "https://reddit.com"
+                },
+                new Proyecto
+                {
+                    Titulo = "Steam",
+                    Descripcion = "Tienda en línea para comprar videojuegos.",
+                    ImagenURL = "/img/steam.png",
+                    Link = "https://store.steampowered.com"
+                }
+            };
+        }
+    }
+}
